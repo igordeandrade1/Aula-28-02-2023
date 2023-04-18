6e91880edf8005a5033990d9df27752d9f7b4f14
igordeandrade1 <116108662+igordeandrade1@users.noreply.github.com>
2023-04-18 15:08:13 -0300

    Dia 1804

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,17 +7,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NovoComponenteComponent } from './paginas/novo-componente/novo-componente.component';
 import { DiretivasComponent } from './diretivas/diretivas.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NovoComponenteComponent,
-    DiretivasComponent
+    DiretivasComponent,
+    HomeComponent,
+    ProdutosComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
